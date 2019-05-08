7fb6462dbd18ad3e37f71f5f19591bdd64e6249c
patelpra337 <patelpra337@gmail.com>
2019-05-07 20:54:53 -0500

    Setup new Screen Project

---
+++ Screens (iOSPT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screens (iOSPT1)
+//
+//  Created by patelpra on 5/7/19.
+//  Copyright © 2019 Pravin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
